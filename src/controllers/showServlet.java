839f5cf75bba5032f8c2717838a97a5d45402254
yuki <my.chiitz@gmail.com>
2020-01-13 21:23:55 +0900

    Add ShowServlet

--- src/controllers/showServlet.java
+++ src/controllers/showServlet.java
@@ -2,41 +2,44 @@ package controllers;
 
 import java.io.IOException;
 
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ShowServlet
- */
+import models.Tasks;
+import utils.DBUtil;
+
+
 @WebServlet("/show")
 public class ShowServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public ShowServlet() {
         super();
-        // TODO Auto-generated constructor stub
+
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        response.getWriter().append("Served at: ").append(request.getContextPath());
-    }
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        doGet(request, response);
+        EntityManager em = DBUtil.createEntityManager();
+
+        //▼該当のIDのタスク1件のみをデータベースから取得
+        Tasks t = em.find(Tasks.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        //▼タスクデータをリクエストスコープにセット（変数"task"にfindで取ってきたtを格納）
+        request.setAttribute("task", t);
+
+        //▼show.jspを呼び出す（フォワード）
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/show.jsp");
+        rd.forward(request, response);
+
     }
 
 }
